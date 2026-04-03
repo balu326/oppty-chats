06f73234bbe51ec7aba8ecde94609a4bda419c11
balu326 <reddybalaji326@gmail.com>
2026-04-03 17:41:12 +0530

    fix: set DJANGO_SETTINGS_MODULE before imports in asgi.py

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,15 +1,14 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.sessions import SessionMiddlewareStack
 from django.core.asgi import get_asgi_application
 
-from chatapp.routing import websocket_urlpatterns
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
 django_asgi_app = get_asgi_application()
+
+from chatapp.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
